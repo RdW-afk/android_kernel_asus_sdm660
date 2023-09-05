f02e51fc7eff61bc4018ee62eccc39e097392b2d
EmanuelCN <emanuelghub@gmail.com>
2023-09-05 16:31:37 +0000

    kernel/sched: Cover more walt cases

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4077,7 +4077,9 @@ void scheduler_tick(void)
 	u64 wallclock;
 	bool early_notif;
 	u32 old_load;
+#ifdef CONFIG_SCHED_WALT
 	struct related_thread_group *grp;
+#endif
 	unsigned int flag = 0;
 
 	sched_clock_tick();
@@ -4108,6 +4110,7 @@ void scheduler_tick(void)
 	trigger_load_balance(rq);
 #endif
 
+#ifdef CONFIG_SCHED_WALT
 	rcu_read_lock();
 	grp = task_related_thread_group(curr);
 	if (update_preferred_cluster(grp, curr, old_load, true))
@@ -4116,6 +4119,7 @@ void scheduler_tick(void)
 
 	if (curr->sched_class == &fair_sched_class)
 		check_for_migration(rq, curr);
+#endif
 
 #ifdef CONFIG_SMP
 	rq_lock(rq, &rf);
